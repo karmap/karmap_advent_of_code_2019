40dbc7812e4cd2b134f20463bb6c234084f4fcc4
karmap <adrian.escalante@mmlafleur.com>
2019-12-04 16:38:47 -0600

    Update 03-part1.py

--- 03/03-part1.py
+++ 03/03-part1.py
@@ -1,10 +1,6 @@
 # R75,D30,R83,U83,L12,D49,R71,U7,L72
 # U62,R66,U55,R34,D71,R55,D58,R83 = distance 159
 
-# R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-# U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
-
-
 def path(instructions):
     coords = instructions.split(',')
     pos = [0,0]
@@ -14,22 +10,38 @@ def path(instructions):
         steps = int(i[1:])
         code = i[0]
         if code == 'U':
-            pos[1] += steps
+            for i in range(0, steps):
+                pos[1] += 1
+                path.append(tuple(pos.copy()))  
         elif code == 'D':
-            pos[1] -= steps
+            for i in range(0, steps):
+                pos[1] -= 1
+                path.append(tuple(pos.copy()))  
         elif code == 'L':
-            pos[0] -= steps
+           for i in range(0, steps):
+                pos[0] -= 1
+                path.append(tuple(pos.copy()))      
         elif code == 'R':
-            pos[0] += steps
-        path.append(tuple(pos.copy()))
-    print(path)
+            for i in range(0, steps):
+                pos[0] += 1
+                path.append(tuple(pos.copy()))
     return path
 
 def closest_intersection(a, b):
-    return set(a).intersection(b)
+    return min(abs(x[0])+abs(x[1]) for x in set(a).intersection(b))
 
 
-wire1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
-wire2 = 'U62,R66,U55,R34,D71,R55,D58,R83'
+# wire1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
+# wire2 = 'U62,R66,U55,R34,D71,R55,D58,R83' # = distance 159
+
+# wire1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51' 
+# wire2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7' # = distance 135
+
+f = open("03-input.txt", "r")
+wire1 = f.readline()
+wire2 = f.readline()
+
+print(wire1)
+print(wire2)
 
 print(closest_intersection(path(wire1), path(wire2)))
